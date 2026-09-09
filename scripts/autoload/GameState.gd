50165d78aeb53b7cfb9a464948fdf3fc990d3a1f
Nick Lo <nilocky@gmail.com>
2026-09-09 15:26:13 +0800

    feat: add GameState autoload with runtime state management

---
+++ scripts/autoload/GameState.gd
@@ -0,0 +1,32 @@
+extends Node
+
+var hp: int = 20
+var max_hp: int = 20
+var money: int = 10
+var round: int = 1
+var bag: Array = []
+var hand: Array = []
+var discard: Array = []
+var current_monster: Dictionary = {}
+var shop_inventory: Array = []
+var active_pack_id: String = ""
+var shield: int = 0
+var extra_draw: int = 0
+
+func reset():
+	hp = 20
+	max_hp = 20
+	money = 10
+	round = 1
+	bag = []
+	hand = []
+	discard = []
+	current_monster = {}
+	shop_inventory = []
+	active_pack_id = ""
+	shield = 0
+	extra_draw = 0
+
+func load_starter_bag(caps: Array):
+	bag = caps.duplicate()
+	bag.shuffle()
