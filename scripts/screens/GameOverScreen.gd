f87aa24ff32d086596e66e70a55e3c3be8611b46
Nick Lo <nilocky@gmail.com>
2026-09-10 13:07:50 +0800

    screens: game over shows round reached; menu copy to V2

--- scripts/screens/GameOverScreen.gd
+++ scripts/screens/GameOverScreen.gd
@@ -1,11 +1,17 @@
 # scripts/screens/GameOverScreen.gd
 extends Control
 
-@onready var final_score_label = $VBoxContainer/FinalScoreLabel
-@onready var restart_button = $VBoxContainer/RestartButton
+@onready var body_label = %BodyLabel
+@onready var restart_button = %RestartButton
+
 
 func _ready():
-	final_score_label.text = "Game Over!\nRound: " + str(GameState.round) + "\nMoney: $" + str(GameState.money)
+	restart_button.pressed.connect(_on_restart_pressed)
+
+
+func show_game_over(reached_round: int) -> void:
+	body_label.text = "Reached round %d with $%d" % [reached_round, GameState.money]
+
 
 func _on_restart_pressed():
 	get_parent()._switch_to_menu()
--- scripts/screens/MainMenuScreen.gd
+++ scripts/screens/MainMenuScreen.gd
@@ -1,20 +1,27 @@
 # scripts/screens/MainMenuScreen.gd
 extends Control
 
-@onready var pack_list = $VBoxContainer/PackList
-@onready var start_button = $VBoxContainer/StartButton
+@onready var pack_list = %PackList
+@onready var start_button = %StartButton
+
 
 func _ready():
-	var packs = PackService.get_available_packs()
-	for pack in packs:
+	start_button.pressed.connect(_on_start_pressed)
+	for pack in PackService.get_available_packs():
 		var btn = Button.new()
-		btn.text = pack["name"] + "\n" + pack["description"]
-		btn.connect("pressed", Callable(self, "_on_pack_selected").bind(pack["id"]))
+		btn.text = pack["name"] + "\n" + pack["desc"]
+		btn.pressed.connect(_on_pack_selected.bind(pack["id"]))
 		pack_list.add_child(btn)
+
 
 func _on_pack_selected(pack_id: String):
 	PackService.select_pack(pack_id)
 	start_button.disabled = false
 
+
 func _on_start_pressed():
+	if GameState.active_pack_id == "":
+		var packs = PackService.get_available_packs()
+		if packs.size() > 0:
+			_on_pack_selected(packs[0]["id"])
 	EventBus.run_started.emit()
